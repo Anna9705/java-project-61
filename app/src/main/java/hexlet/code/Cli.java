d0536f5cb8c06e38c7ad036e807ace511d6f70cb
Anna Zakharova <zaharova9705@gmail.com>
2022-09-29 14:08:04 +0300

    add greeting and Makefile

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greetPlayer() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String name = input.next();
+        System.out.println("Hello, " + name +"!");
+    }
+}
