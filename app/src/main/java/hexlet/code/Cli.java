56d9ea591aff13c2583bb61729ebf18a51386603
Anna Zakharova <zaharova9705@gmail.com>
2022-10-04 12:01:39 +0300

    fixed Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,13 +1,8 @@
 package hexlet.code;
 
-import java.util.Scanner;
 
 public class Cli {
     public static void greetPlayer() {
         System.out.println("\nWelcome to the Brain Games!");
-        Scanner input = new Scanner(System.in);
-        System.out.print("May I have your name? ");
-        String name = input.next();
-        System.out.println("Hello, " + name + "!");
     }
 }
